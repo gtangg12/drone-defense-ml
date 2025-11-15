97442329222b3872a7fa62896e2179485b8c07a4
George Tang <gtangg12@gmail.com>
2025-11-15 11:23:29 -0800

    DinoV2

--- src/dronedefense/models.py
+++ src/dronedefense/models.py
@@ -1,30 +1,11 @@
-# import torch
-# from transformers import AutoImageProcessor, AutoModel
-# from transformers.image_utils import load_image
-
-# url = "http://images.cocodataset.org/val2017/000000039769.jpg"
-# image = load_image(url)
-
-# pretrained_model_name = "facebook/dinov3-convnext-tiny-pretrain-lvd1689m"
-# processor = AutoImageProcessor.from_pretrained(pretrained_model_name)
-# model = AutoModel.from_pretrained(
-#     pretrained_model_name, 
-#     device_map="auto", 
-# )
-
-# inputs = processor(images=image, return_tensors="pt").to(model.device)
-# with torch.inference_mode():
-#     outputs = model(**inputs)
-
-# pooled_output = outputs.pooler_output
-# print("Pooled output shape:", pooled_output.shape)
-
 from typing import Optional
 
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
+from sklearn.decomposition import PCA
+from torchvision import transforms
 from torchvision.ops import masks_to_boxes
 from torchvision.utils import draw_segmentation_masks
 
@@ -32,6 +13,57 @@ import sys
 sys.path.append('./third_party/FastSAM')
 from fastsam import FastSAM, FastSAMPrompt
 sys.path.pop()
+
+
+class ModelDinoV2:
+    PATCH_SIZE = 14
+
+    def __init__(
+        self,
+        backbone: str = 'dinov2_vits14',
+        downsample_factor: float = 2,
+        device=None,
+    ):
+        self.model = torch.hub.load('facebookresearch/dinov2', backbone)
+        self.model.eval()
+        self.device = device or ("cuda" if torch.cuda.is_available() else "cpu")
+        self.model.to(self.device)
+        self.downsample_factor = downsample_factor
+        self.transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
+
+    def __call__(self, image: np.ndarray):
+        H, W = image.shape[:2]
+        round_dim = lambda x: x // self.PATCH_SIZE * self.PATCH_SIZE
+        inH = round_dim(int(H / self.downsample_factor))
+        inW = round_dim(int(W / self.downsample_factor))
+
+        image = self.transform(image)
+        image = transforms.functional.resize(image, (inH, inW))
+        image = image.unsqueeze(0).to(self.device)
+
+        with torch.no_grad():
+            embeddings = self.model.forward_features(image)['x_norm_patchtokens']
+        embeddings = embeddings / torch.norm(embeddings, dim=-1, keepdim=True)
+        embeddings = embeddings.reshape(
+            inH // self.PATCH_SIZE,
+            inW // self.PATCH_SIZE,
+            -1,
+        )
+        return embeddings
+
+    @classmethod
+    def visualize_pca(cls, features: torch.Tensor, n_components: int = 3):
+        H, W, D = features.shape
+        features = features.cpu().numpy().reshape(-1, D)
+        pca = PCA(n_components=n_components)
+        pca_features = pca.fit_transform(features)
+        pca_features = pca_features.reshape(H, W, n_components)
+        pca_features = (pca_features - pca_features.min()) / (pca_features.max() - pca_features.min())
+        pca_features = (pca_features * 255).astype(np.uint8)
+        return Image.fromarray(pca_features)
 
 
 class ModelFastSAM:
@@ -76,16 +108,25 @@ class ModelFastSAM:
         for mask, color in zip(bmasks, colors):
             output[mask] = output[mask] * 0.5 + color * 0.5
         return Image.fromarray(output.astype(np.uint8))
-        
+
 
 if __name__ == '__main__':
-    model = ModelFastSAM("/Users/gtangg12/Desktop/drone-defense-ml/checkpoints/FastSAM-x.pt")
+    import time
+
     image = np.array(Image.open("/Users/gtangg12/Desktop/drone-defense-ml/assets/drones.png").convert("RGB"))
 
-    import time
-    start_time = time.time()
-    bmasks, bboxes = model(image)
-    print(f"Inference time: {time.time() - start_time:.2f} seconds")
+    model = ModelDinoV2(backbone='dinov2_vits14', downsample_factor=2)
+    for i in range(5):
+        start_time = time.time()
+        features = model(image)
+        print(f"Inference time: {time.time() - start_time:.2f} seconds")
+    output = ModelDinoV2.visualize_pca(features, n_components=3)
+    output.save("/Users/gtangg12/Desktop/drone-defense-ml/assets/drones_dino.png")
 
+    model = ModelFastSAM("/Users/gtangg12/Desktop/drone-defense-ml/checkpoints/FastSAM-x.pt")
+    for i in range(5):
+        start_time = time.time()
+        bmasks, bboxes = model(image)
+        print(f"Inference time: {time.time() - start_time:.2f} seconds")
     output = ModelFastSAM.visualize_masks(image, bmasks)
     output.save("/Users/gtangg12/Desktop/drone-defense-ml/assets/drones_masks.png")
---
+++ src/dronedefense/tracker.py
@@ -0,0 +1,9 @@
+from PIL import Image
+
+
+class Tracker:
+    def __init__(self):
+        pass
+
+    def update(self, frame: Image.Image):
+        pass
